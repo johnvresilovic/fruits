0d165ace0609bc273b52724f4b578cbb7e87a39a
John Vresilovic <johnvresilovic@protonmail.com>
2022-12-06 15:42:25 -0500

    working toward integration with MongoDB

--- server.js
+++ server.js
@@ -1,52 +1,67 @@
 require('dotenv').config()
-const express = require('express');
-const app = express();
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
 const port = 3000;
-const fruits = require('./models/fruits.js');
+const Fruit = require('./models/fruits.js')
 
-//Middleware
+//Set up middleware
 app.use((req, res, next) => {
-    console.log('All play and no work makes Jacky a happy boy');
-    next();
+    console.log('All play and no work makes Jacky a happy boy')
+    next()
 });
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:false}))
 
 //Set up view engine
-app.set('view engine', 'jsx');
-  app.engine('jsx', require('express-react-views').createEngine());
+app.set('view engine', 'jsx')
+  app.engine('jsx', require('express-react-views').createEngine())
+
+//Set up Mongoose
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connection.once('open', ()=> {
+      console.log('connected to mongo')
+  })
 
 //INDUCES begins here
 //Index
   app.get('/fruits', function(req, res){
-    res.render('Index', { fruits: fruits });
-});
+    Fruit.find({}), (error, allFruits)=> {
+        res.render('Index', {
+            fruits: allFruits
+        })
+    }
+})
 
 //New - retrieve a form to create a new record - HTML get
 app.get('/fruits/new', (req, res) => {
-    res.render('New');
-});
-
-app.post('/fruits', (req, res)=>{
-    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
-        req.body.readyToEat = true;
-    } else { //if not checked, req.body.readyToEat is undefined
-        req.body.readyToEat = false;
-    }
-    fruits.push(req.body);
-    res.redirect('/fruits'); //send the user back to /fruits
-});
+    res.render('New')
+})
 
 //Delete
+
 //Update - update record from db - HTML post
+
 //Create - send filled out form to db to create new record - HTML post
+app.post('/fruits/', (req, res)=>{
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false
+    }
+    Fruit.create(req.body, (error, createdFruit)=>{
+        res.send(createdFruit);
+    })
+})
+
 //Edit - retrieve record to be updated from db - HTML get
 
 //Show
-app.get('/fruits/:indexOfFruitsArray', function(req, res){
-    res.render('Show', {fruit: fruits[req.params.indexOfFruitsArray] 
+app.get('/fruits/:id', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{
+        res.send(foundFruit);
     });
-});
+})
 
 app.listen(port, () => {
-    console.log(port + ' is listening');
-});
+    console.log(port + ' is listening')
+})
